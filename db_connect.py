26752cea62e332e712f02a744beb3c57d4be75ea
Ushnah Abbasi <90151235+UshnahAbbasi@users.noreply.github.com>
2023-06-22 17:13:54 +0500

    Update db_connect.py
    made some supporting functions for all the tasks associated with database.

--- db_connect.py
+++ db_connect.py
@@ -1,4 +1,5 @@
 
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
@@ -22,3 +23,25 @@ def db_connect():
         password=db_password
         )
     return conn
+
+def get_data(query):
+    conn=db_connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    print(data)
+    cursor.close()
+    return data
+
+def get_query(timestamp, hour, minute, second):
+    if timestamp != None:
+        query = f"SELECT * FROM memory_usage WHERE timestamp = {timestamp}"
+    else:
+        if hour != None:
+            query = f"SELECT * FROM memory_usage WHERE hour = {hour}"
+        if minute != None:
+            query = f"SELECT * FROM memory_usage WHERE hour = {hour} and minute ={minute}"
+        if second != None:
+            query = f"SELECT * FROM memory_usage WHERE hour = {hour} and minute ={minute} and second={second} "
+    
+    return query
