db6d8e0268b0f5a1725d857d5e7f2023412feeb9
Ushnah Abbasi <90151235+UshnahAbbasi@users.noreply.github.com>
2023-06-22 16:46:58 +0500

    Update API_mem_usage.py

--- API_mem_usage.py
+++ API_mem_usage.py
@@ -1,37 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 12 17:32:39 2023
-
-@author: ushnah.abbasi
-"""
-
 from flask import Flask, jsonify,request
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from db_connect import db_connect
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(
-    host='localhost',
-    port='5432',
-    database='db_ushnah',
-    user='ushnah',
-    password='abbasi'
-    )
 
-
-# ... Code to set up database connection and other configurations ...
 @app.route('/usage', methods=['GET'])
 def get_usage():
     timestamp = request.args.get('timestamp')
     #idd=request.args.get('idd')
     print(timestamp)
-    # Perform a database query to retrieve the usage at the provided timestamp
-    # You can adjust the query based on the structure of your 'dummy_data' table
+   
     query = f"SELECT * FROM memory_usage WHERE timestamp = {timestamp}"
     #query = f"SELECT * FROM dummy_data WHERE id = '{idd}'"
     print(query)
+    conn=db_connect()
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
