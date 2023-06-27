9e32b382b45b73a1856c8dac7b3aaefa11449f88
Ushnah Abbasi <90151235+UshnahAbbasi@users.noreply.github.com>
2023-06-28 02:39:00 +0500

    Update API_mem_usage.py

--- API_mem_usage.py
+++ API_mem_usage.py
@@ -4,31 +4,37 @@ from db_connect import get_data, get_query
 app = Flask(__name__)
 
 
-@app.route('/usage', methods=['GET'])
+@app.route('/usage', methods=['POST'])
 def get_usage():
-    timestamp = request.args.get('timestamp')
-
-    print(timestamp)
     
-    hour = request.args.get('hour')
-    print(hour)
-
-    minute = request.args.get('minute')
-    print(minute)
-
-    second = request.args.get('second')
-    print(second)
-
+    data = request.get_json()
+    print(data)
     
+    
+    timestamp = data.get('timestamp')
+    hour = data.get('hour')
+    minute = data.get('minute')
+    second = data.get('second')
+  
     query=get_query(timestamp, hour, minute, second)
         
 
-    print(query)
+    #print(query)
     data= get_data(query)
     if data == []:
         return "no data available at this time"
     else:
-        return jsonify(data)
+        
+        keys = ['id', 'timestamp', 'memory_footprint', 'cpu_usage', 'hour', 'minute', 'second']
+        
+        result = []
+        for sublist in data:
+            dictionary = dict(zip(keys, sublist))
+            result.append(dictionary)
+            
+        print(result)
+            
+        return jsonify(result)
 
 
 if __name__ == '__main__':
